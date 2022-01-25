c70da52a4a48288b8a5c2dff43a6442c69abd4fd
matheus-fernando <matheus.fernando.coder.com>
2022-01-25 13:51:00 -0300

    Today the class was about setstate and I've lurned how to change a state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-function App() {
-  return (
-    <div className="App"></div>
-  );
+// function App() {
+//   return (
+//     <div className="App"><h1>Hello, world</h1></div>
+//   );
+// }
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handlerPClick = this.handlerPClick.bind(this);
+    this.handlerAClick = this.handlerAClick.bind(this);
+
+    this.state = {
+      name: 'Matheus',
+      counter: 0
+    };
+  }
+
+  handlerPClick() {
+      this.setState({ name: 'Fernando' });
+  }
+
+  handleAClick() {
+    const { counter } = this.state;
+    this.setState({counter: counter + 1});
+  }
+
+  render() {
+    const { name } = this.state;
+    const { counter } = this.state;
+
+    return(
+      <div className='mainContainer'>
+          <p onClick={this.handlerPClick}>
+            {name}
+          </p>
+          <a 
+             onClick={this.handlerAClick}
+             href="https://www.google.com/search?q=digimon&sxsrf=AOaemvJxi4CvTIvC8x7lJIRmMsFFdliDWg:1643119624153&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjB7pTMic31AhUKqpUCHYApAo4Q_AUoAnoECAEQBA&biw=1366&bih=657&dpr=1#imgrc=J62FAEjUv85UtM"
+             target={'_blank'}
+          >
+            Digimon é maior que pokemon
+          </a>
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
