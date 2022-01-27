71216cb20c88d7c4090ee6ead01130245c4835a8
matheus-fernando <matheus.fernando.coder@gmail.com>
2022-01-27 11:38:57 -0300

    Today I learned about lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
 import './index.css';
-import { Component } from 'react';
+import { Component, useState } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handlerPClick = this.handlerPClick.bind(this);
-    this.handlerAClick = this.handlerAClick.bind(this);
-
-    this.state = {
-      name: 'Matheus',
-      counter: 0
-    };
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id:1,
+        title: 'Title 1',
+        body: 'body 1'
+      },
+      {
+        id:2,
+        title: 'Title 2',
+        body: 'body 2'
+      },
+      {
+        id:3,
+        title: 'Title 3',
+        body: 'body 3'
+      },
+    ]
   }
 
-  handlerPClick() {
-      this.setState({ name: 'Fernando' });
+  componentDidMount() {
+    this.handleTimeOut();
   }
 
-  handlerAClick(event) {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter + 1});
+  handleTimeOut() {
+    const { posts, counter } = this.state;
+    posts[0].title = 'The original title has changed!';
+
+    setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 5000);
   }
 
   render() {
-    const { name, counter } = this.state;
+    const { posts, counter } = this.state;
 
     return(
-      <div className='mainContainer'>
-          <p onClick={this.handlerPClick}>
-            {name} {counter}
-          </p>
-          <a 
-             onClick={this.handlerAClick}
-             href="https://www.google.com/search?q=digimon&sxsrf=AOaemvJxi4CvTIvC8x7lJIRmMsFFdliDWg:1643119624153&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjB7pTMic31AhUKqpUCHYApAo4Q_AUoAnoECAEQBA&biw=1366&bih=657&dpr=1#imgrc=J62FAEjUv85UtM"
-             target={'_blank'}
-          >
-            Digimon é maior que pokemon
-          </a>
+      <div className='App'>
+        <h1>{counter}</h1>
+        {posts.map(post => (
+           <div className='noNameYet' key={post.id}>
+             <h1 >{post.title}</h1>
+             <p>{post.body}</p>
+           </div>
+        ))}             
       </div>
     );
   }
-
-  
 }
 
 export default App;
